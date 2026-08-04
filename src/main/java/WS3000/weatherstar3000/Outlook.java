a4f69d5b1b43b40271d07c079e5bd14a03fe15dc
JoeMolinelli <116307221+TheGoldDiamond9@users.noreply.github.com>
2026-08-04 17:50:09 -0400

    Add files via upload

---
+++ src/main/java/WS3000/weatherstar3000/Outlook.java
@@ -0,0 +1,47 @@
+package WS3000.weatherstar3000;
+
+//import java.net.URI;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+
+import util.Utilities;
+
+//import org.json.JSONObject;
+
+public class Outlook extends Thread {
+	static Utilities utl = new Utilities();
+	String month;
+	ArrayList<String> outlook = new ArrayList<>();
+	String getMonth() {
+		try {
+		LocalDate cdate = LocalDate.now();
+        Month cmonth = cdate.getMonth();
+        
+        return cmonth.name().toUpperCase();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+    }
+	ArrayList<String> getOutlook() {
+		ArrayList<String> outlook = new ArrayList<>();
+		try {
+		outlook.add("NO");
+		outlook.add("REPORT");
+		return outlook;
+		} catch (Exception e) {
+			e.printStackTrace();
+			outlook.add("NO");
+			outlook.add("REPORT");
+			return outlook;
+		}
+	}
+	public void run() {
+		Main.outlook.month = getMonth();
+		Main.outlook.outlook = getOutlook();
+	}
+}
