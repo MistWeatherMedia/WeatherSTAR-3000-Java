a29f4d08295642a53e538c17b51d3210bb664e1b
JoeMolinelli <116307221+TheGoldDiamond9@users.noreply.github.com>
2026-08-04 23:54:09 -0400

    Delete src directory

--- src/main/java/WS3000/weatherstar3000/WindowRunner.java
+++
@@ -1,9 +0,0 @@
-package WS3000.weatherstar3000;
-
-public class WindowRunner extends Thread {
-	public void run() {
-		Main.settingsWindow.setVisible(false);
-		Window window = Window.get();
-		window.runWindow();
-	}
-}
