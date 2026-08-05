742714fbacf04132e1b5dafcc2db6d3f68089d26
JoeMolinelli <116307221+TheGoldDiamond9@users.noreply.github.com>
2026-08-04 23:54:29 -0400

    Add files via upload

---
+++ src/main/java/components/SpriteRenderer.java
@@ -0,0 +1,73 @@
+package components;
+
+import WS3000.weatherstar3000.Component;
+import WS3000.weatherstar3000.Transform;
+import renderer.Texture;
+
+import org.joml.Vector2f;
+import org.joml.Vector4f;
+
+public class SpriteRenderer extends Component {
+	private Vector4f color;
+	private Sprite sprite;
+	
+	private Transform lastTransform;
+	private boolean isDirty;
+	
+	public SpriteRenderer(Vector4f color) {
+		this.color = color;
+		this.sprite = new Sprite(null);
+		this.isDirty = true;
+	}
+	
+	public SpriteRenderer(Sprite sprite) {
+		this.sprite = sprite;
+		this.color = new Vector4f(1,1,1,1);
+		this.isDirty = true;
+	}
+	
+	@Override
+	public void start() {
+		this.lastTransform = gameObject.transform.copy();
+	}
+	
+	@Override
+	public void update(float dt) {
+		if (!this.lastTransform.equals(this.gameObject.transform)) {
+			this.gameObject.transform.copy(lastTransform);
+			isDirty = true;
+		}
+	}
+	
+	public Vector4f getColor() { 
+		return this.color;
+	}
+	
+	public Texture getTexture() {
+		return sprite.getTexture();
+	}
+	
+	public Vector2f[] getTexCoords() {
+		return sprite.getTexCoords();
+	}
+	
+	public void setSprite(Sprite sprite) {
+		this.sprite = sprite;
+		this.isDirty = true;
+	}
+	
+	public void setColor(Vector4f color) {
+		if (!this.color.equals(color)) {
+			this.isDirty = true;
+			this.color = color;
+		}
+	}
+	
+	public boolean isDirty() {
+		return this.isDirty;
+	}
+	
+	public void setClean() {
+		this.isDirty = false;
+	}
+}
